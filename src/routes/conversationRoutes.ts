1bbd80f1cd729dd1d4e8387fcc15483ba93b6bf1
Adriano <adriano.branco@etu.univ-lyon1.fr>
2023-11-15 17:21:32 +0100

    add authentification at conversationRoutes and messageRoutes

--- src/routes/conversationRoutes.ts
+++ src/routes/conversationRoutes.ts
@@ -1,11 +1,12 @@
+import checkAuth from "../auth";
 const express = require('express');
 const conversationRoutesController = require('./Controller/conversationRoutesController');
 const conversationRoutes = express.Router();
 
-conversationRoutes.post('/conversations/create', conversationRoutesController.createConversation);
-conversationRoutes.get('/conversations/', conversationRoutesController.getAllConversations);
-conversationRoutes.delete('/conversations/:id', conversationRoutesController.deleteConversation);
-conversationRoutes.post('/conversations/see/:id', conversationRoutesController.setConversationSeen);
-conversationRoutes.post('/conversations/:id', conversationRoutesController.addMessageToConversation);
+conversationRoutes.post('/conversations/create', checkAuth, conversationRoutesController.createConversation);
+conversationRoutes.get('/conversations/', checkAuth, conversationRoutesController.getAllConversations);
+conversationRoutes.delete('/conversations/:id', checkAuth, conversationRoutesController.deleteConversation);
+conversationRoutes.post('/conversations/see/:id', checkAuth, conversationRoutesController.setConversationSeen);
+conversationRoutes.post('/conversations/:id', checkAuth, conversationRoutesController.addMessageToConversation);
 
 module.exports = conversationRoutes;
--- src/routes/messageRoutes.ts
+++ src/routes/messageRoutes.ts
@@ -1,10 +1,11 @@
+import checkAuth from "../auth";
 const express = require('express');
 const messageRoutesController = require('./Controller/messageRoutesController');
 
 const messageRoutes = express.Router();
 
-messageRoutes.put('/messages/:id', messageRoutesController.editMessage);
-messageRoutes.post('/messages/:id/react', messageRoutesController.reactToMessage);
-messageRoutes.delete('/messages/:id', messageRoutesController.deleteMessage);
+messageRoutes.put('/messages/:id', checkAuth, messageRoutesController.editMessage);
+messageRoutes.post('/messages/:id/react', checkAuth, messageRoutesController.reactToMessage);
+messageRoutes.delete('/messages/:id', checkAuth, messageRoutesController.deleteMessage);
 
 module.exports = messageRoutes;
