935d358877f0b41e1617892b64cac5a177d59d6d
Adriano <adriano.branco@etu.univ-lyon1.fr>
2023-11-15 15:02:03 +0100

    add JoiRequestValidator

--- src/JoiRequestValidator.ts
+++ src/JoiRequestValidator.ts
@@ -1,57 +1,102 @@
-import * as joi from "joi";
-import { Request } from "express";
+import * as joi from 'joi';
+import { Request } from 'express';
 
-interface JoiRequestValidatorResponse
-{
-	error?: string
+interface JoiRequestValidatorResponse {
+  error?: string;
 }
 
-interface JoiRouteValidator
-{
-	route: string,
-	method: string,
-	validatorSchema: joi.ObjectSchema<any>
+interface JoiRouteValidator {
+  route: string;
+  method: string;
+  validatorSchema: joi.ObjectSchema<any>;
 }
 
-class JoiRequestValidator 
-{
-	validators: JoiRouteValidator[] = 
-	[
-		// EXEMPLE
-		// {
-		// 	route: "/conversations/:id",
-		// 	method: "POST",
-		// 	validatorSchema: bodyFormat,
-		// }
-	];
+class JoiRequestValidator {
+  validators: JoiRouteValidator[] = [
+    {
+      route: '/users/login',
+      method: 'POST',
+      validatorSchema: joi.object({
+        username: joi.string().required(),
+        password: joi.string().required(),
+      }),
+    },
+    {
+      route: '/conversations',
+      method: 'POST',
+      validatorSchema: joi.object({
+        concernedUsersIds: joi.array().items(joi.string()).min(2).required(),
+      }),
+    },
+    {
+      route: '/conversations',
+      method: 'GET',
+      validatorSchema: joi.object({}),
+    },
+    {
+      route: '/conversations/:id',
+      method: 'DELETE',
+      validatorSchema: joi.object({}),
+    },
+    {
+      route: '/conversations/see/:id',
+      method: 'POST',
+      validatorSchema: joi.object({
+        messageId: joi.string().required(),
+      }),
+    },
+    {
+      route: '/conversations/:id',
+      method: 'POST',
+      validatorSchema: joi.object({
+        content: joi.string().required(),
+        messageReplyId: joi.string(),
+      }),
+    },
+    {
+      route: '/messages/:id',
+      method: 'PUT',
+      validatorSchema: joi.object({
+        newMessageContent: joi.string().required(),
+      }),
+    },
+    {
+      route: '/messages/:id',
+      method: 'POST',
+      validatorSchema: joi.object({
+        reaction: joi.string().valid('HAPPY', 'SAD', 'THUMBSUP', 'THUMBSDOWN', 'LOVE').required(),
+      }),
+    },
+    {
+      route: '/messages/:id',
+      method: 'DELETE',
+      validatorSchema: joi.object({}),
+    },
+    // Ajoutez d'autres validateurs pour d'autres routes
+  ];
 
-	validate(request: Request): JoiRequestValidatorResponse 
-	{
-		// request.baseUrl contient l'URL de base, avant application des middlewares.
-		// request.route.path contient l'URL que vous déclarez dans votre middleware de routage.
-		console.log(request.baseUrl);
-		console.log(request.route.path);
+  validate(request: Request): JoiRequestValidatorResponse {
+    const route = request.route ? request.route.path : '';
+    const method = request.method?.toUpperCase();
 
-		/* 
-			ETAPE 1:
+    const validator = this.validators.find(
+      (v) => v.route === route && v.method === method
+    );
 
-			Trouver dans la liste de validators, le validator qui correspond à la route de la requête.
-		*/
+    if (!validator) {
+      return {}; // Aucun validateur trouvé, la validation n'est pas nécessaire
+    }
 
-		/* 
-			ETAPE 2:
+    const validationResult = validator.validatorSchema.validate(request.body);
 
-			Si le validator n'existe pas
-				=> retourner un objet vide.
-			Si le validator existe 
-				=> valider le body de la requête.
-				=> Si le body est valide
-					=> retourner un objet vide.
-				=> Si le body est invalide
-					=> retourner un objet avec une clé error contenant les details de l'erreur.
-		*/
-		return {};
-	}
+    if (validationResult.error) {
+      return {
+        error: validationResult.error.details.map((d) => d.message).join(', '),
+      };
+    }
+
+    return {};
+  }
 }
 
 export const JoiRequestValidatorInstance = new JoiRequestValidator();
