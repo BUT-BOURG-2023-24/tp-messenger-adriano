a8bf820d1f95a79a6133aca4397b2963accc51fd
Adriano <adriano.branco@etu.univ-lyon1.fr>
2023-11-15 15:37:31 +0100

    modify JoiRequestValidator

--- src/JoiRequestValidator.ts
+++ src/JoiRequestValidator.ts
@@ -2,101 +2,106 @@ import * as joi from 'joi';
 import { Request } from 'express';
 
 interface JoiRequestValidatorResponse {
-  error?: string;
+	error?: string;
 }
 
 interface JoiRouteValidator {
-  route: string;
-  method: string;
-  validatorSchema: joi.ObjectSchema<any>;
+	route: string;
+	method: string;
+	validatorSchema: joi.ObjectSchema<any>;
 }
 
 class JoiRequestValidator {
-  validators: JoiRouteValidator[] = [
-    {
-      route: '/users/login',
-      method: 'POST',
-      validatorSchema: joi.object({
-        username: joi.string().required(),
-        password: joi.string().required(),
-      }),
-    },
-    {
-      route: '/conversations',
-      method: 'POST',
-      validatorSchema: joi.object({
-        concernedUsersIds: joi.array().items(joi.string()).min(2).required(),
-      }),
-    },
-    {
-      route: '/conversations',
-      method: 'GET',
-      validatorSchema: joi.object({}),
-    },
-    {
-      route: '/conversations/:id',
-      method: 'DELETE',
-      validatorSchema: joi.object({}),
-    },
-    {
-      route: '/conversations/see/:id',
-      method: 'POST',
-      validatorSchema: joi.object({
-        messageId: joi.string().required(),
-      }),
-    },
-    {
-      route: '/conversations/:id',
-      method: 'POST',
-      validatorSchema: joi.object({
-        content: joi.string().required(),
-        messageReplyId: joi.string(),
-      }),
-    },
-    {
-      route: '/messages/:id',
-      method: 'PUT',
-      validatorSchema: joi.object({
-        newMessageContent: joi.string().required(),
-      }),
-    },
-    {
-      route: '/messages/:id',
-      method: 'POST',
-      validatorSchema: joi.object({
-        reaction: joi.string().valid('HAPPY', 'SAD', 'THUMBSUP', 'THUMBSDOWN', 'LOVE').required(),
-      }),
-    },
-    {
-      route: '/messages/:id',
-      method: 'DELETE',
-      validatorSchema: joi.object({}),
-    },
-    // Ajoutez d'autres validateurs pour d'autres routes
-  ];
+	validators: JoiRouteValidator[] = [
+		{
+			route: '/users/login',
+			method: 'POST',
+			validatorSchema: joi.object({
+				username: joi.string().required(),
+				password: joi.string().required(),
+			}),
+		},
+		{
+			route: "/users/online",
+			method: "GET",
+			validatorSchema: joi.object({}),
+		},
+		{
+			route: '/conversations',
+			method: 'POST',
+			validatorSchema: joi.object({
+				concernedUsersIds: joi.array().items(joi.string()).min(2).required(),
+			}),
+		},
+		{
+			route: '/conversations',
+			method: 'GET',
+			validatorSchema: joi.object({}),
+		},
+		{
+			route: '/conversations/:id',
+			method: 'DELETE',
+			validatorSchema: joi.object({}),
+		},
+		{
+			route: '/conversations/see/:id',
+			method: 'POST',
+			validatorSchema: joi.object({
+				messageId: joi.string().required(),
+			}),
+		},
+		{
+			route: '/conversations/:id',
+			method: 'POST',
+			validatorSchema: joi.object({
+				content: joi.string().required(),
+				messageReplyId: joi.string(),
+			}),
+		},
+		{
+			route: '/messages/:id',
+			method: 'PUT',
+			validatorSchema: joi.object({
+				newMessageContent: joi.string().required(),
+			}),
+		},
+		{
+			route: '/messages/:id',
+			method: 'POST',
+			validatorSchema: joi.object({
+				reaction: joi.string().valid('HAPPY', 'SAD', 'THUMBSUP', 'THUMBSDOWN', 'LOVE').required(),
+			}),
+		},
+		{
+			route: '/messages/:id',
+			method: 'DELETE',
+			validatorSchema: joi.object({}),
+		},
+		// Ajoutez d'autres validateurs pour d'autres routes
+	];
 
-  validate(request: Request): JoiRequestValidatorResponse {
-    const route = request.route ? request.route.path : '';
-    const method = request.method?.toUpperCase();
+	validate(request: Request): JoiRequestValidatorResponse {
+		const route = request.route ? request.route.path : '';
+		const method = request.method?.toUpperCase();
 
-    const validator = this.validators.find(
-      (v) => v.route === route && v.method === method
-    );
+		const validator = this.validators.find(
+			(v) => v.route === route && v.method === method
+		);
 
-    if (!validator) {
-      return {}; // Aucun validateur trouvé, la validation n'est pas nécessaire
-    }
+		if (!validator) {
+			return {}; // Aucun validateur trouvé, la validation n'est pas nécessaire
+		}
 
-    const validationResult = validator.validatorSchema.validate(request.body);
+		const validationResult = validator.validatorSchema.validate(request.body);
 
-    if (validationResult.error) {
-      return {
-        error: validationResult.error.details.map((d) => d.message).join(', '),
-      };
-    }
+		if (validationResult.error) {
+			return {
+				error: validationResult.error.details.map((d) => d.message).join(', '),
+			};
+		}
 
-    return {};
-  }
+		return {};
+	}
 }
 
 export const JoiRequestValidatorInstance = new JoiRequestValidator();
